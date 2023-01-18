1f143d691d7793a0182b138405729651ddc16cbd
RosLaffey <neptsun@163.com>
2023-01-18 20:06:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+import sys
+
+def gen(n:int) -> str:
+    dic=['形不成形，意不在意，再去练练吧','千招百式在一息','劲发江潮落，气收秋毫平','你们解决问题，还是只会仰仗干戈吗']
+    s=''
+    for i in range(n):
+        tmp=random.choice(dic)
+        s+=tmp[:-(int(random.random()*len(tmp)))]
+    s+=random.choice(dic)
+    return s
+
+if __name__=="__main__":
+    if len(sys.argv)==1:
+        print('usage: python3 main.py (number)')
+    else:
+        print(gen(int(sys.argv[1])))
